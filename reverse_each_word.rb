1b67d75b82c421cd8ae4d2697e14ec802ff371ca
Cedric Patton <ceddyp117@gmail.com>
2020-07-03 06:23:37 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  reversed_sentence = []
+  
+  sentence.reverse.split(" ").each do |word|
+    reversed_sentence.unshift(word)
+  end
+  reversed_sentence.join(" ")
+end
+
+
+
+def reverse_each_word(sentence)
+  sentence.reverse.split(" ").collect do |word|
+    word.join(" ")
+  end 
+end
